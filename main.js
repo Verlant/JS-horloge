08e0d5c6192ceecefcbdc24d5dc12e256dad9e7e
Verlant <antoine.verlyck@gmail.com>
2022-12-08 15:30:12 +0100

    exo termine

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class Horloge {
     this.seconde = seconde;
   }
   /**
-   * ajoute 21 minute et 28 secondes aux attributs de l'objet Horloge
+   * ajoute 21 minute et 28 secondes aux attributs de l'objet Horloge (avec if)
    */
   go_to_future(min, sec) {
     this.minute += min;
@@ -46,6 +46,18 @@ class Horloge {
   print_attributs() {
     console.log(this.hour + " : " + this.minute + " : " + this.seconde);
   }
+  /**
+   * ajoute des minute et seconde au temps en convertissant tout en sec puis en h : m : s (sans if)
+   * @param {int} min
+   * @param {int} sec
+   */
+  go_to_future_2(min, sec) {
+    let time_in_sec =
+      this.hour * 3600 + (this.minute + min) * 60 + this.seconde + sec;
+    this.hour = Math.floor(time_in_sec / 3600);
+    this.minute = Math.floor((time_in_sec % 3600) / 60);
+    this.seconde = (time_in_sec % 3600) % 60;
+  }
 }
 
 const TODAY = new Date();
@@ -60,7 +72,30 @@ document.body.append(BTN);
 BTN.textContent =
   "21 minutes et 28 secondes plus tard (pas le film sur les zombies)";
 
+const P = document.createElement("p");
+document.body.append(P);
+
 BTN.addEventListener("click", (e) => {
-  HORLOGE.go_to_future(21, 28);
+  HORLOGE.go_to_future_2(21, 28);
+  HORLOGE.print_attributs();
 });
-console.log(BTN);
+
+/**
+ * affiche l'heure de l'objet horloge dans une balise p
+ */
+function affiche() {
+  P.textContent =
+    HORLOGE.hour + " heures " + HORLOGE.minute + " m " + HORLOGE.seconde + " s";
+  HORLOGE.seconde++;
+  if (HORLOGE.seconde == 60) {
+    HORLOGE.seconde = 0;
+    HORLOGE.minute++;
+  } else if (HORLOGE.minute == 60) {
+    HORLOGE.minute = 0;
+    HORLOGE.hour++;
+  } else if (HORLOGE.hour == 24) {
+    HORLOGE.hour = 0;
+  }
+}
+
+setInterval(affiche, 1000);
